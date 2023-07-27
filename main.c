c21042f297a565fd23c17a27cd02f2c55317b398
Roee <roibenezra0@gmail.com>
2023-07-27 10:07:21 +0300

    insert data to school system

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 #define NAME_LEN 32
-#define PHONE_LEN 11
+#define PHONE_LEN 24
 
-#define NUM_COURSES 20
+#define NUM_COURSES 10
 #define NUM_LEVELS 12
 #define NUM_CLASSES 10
 
@@ -15,59 +15,79 @@
 struct Course {
     char _course_name[NAME_LEN];
     int _grade;
-    bool is_register;
+    bool _is_register;
 };
 
 struct Student {
-    char _name[NAME_LEN];
-    char _phone_number[PHONE_LEN];
+    char *_first_name;
+    char *_last_name;
+    char *_phone_number;
     struct Course _courses_list[NUM_COURSES];
-    int class_number;
-    int class_level;
+    int _class_number;
+    int _class_level;
     struct Student *_next;
-    struct Student *_prev;
+//    struct Student *_prev;
 };
 
 struct Class {
-    int class_number;
-    struct Student first_stud;
+    int _class_number;
+    struct Student *_head_stud_list;
+    struct Student *_tail_stud_list;
 };
 
 struct Level {
-    int class_level;
-    struct Class classes[NUM_CLASSES];
+    int _class_level;
+    struct Class _classes[NUM_CLASSES];
 };
 
 struct School {
-    struct Level levels[NUM_LEVELS];
+    struct Level _levels[NUM_LEVELS];
 };
 
-void open_file(FILE *fptr) {
+
+FILE *open_file() {
     // Open a file in read mode
-    fptr = fopen("/Users/roeebenezra/CLionProjects/Checkpoint/students_with_class.txt", "r");
+    FILE *fptr = fopen("/Users/roeebenezra/CLionProjects/Checkpoint/students_with_class.txt", "r");
     if (fptr == NULL) {
         printf("file isn't opened");
         exit(EXIT_FAILURE);
     }
+    return fptr;
 }
+
 
 static struct School school_system;
 
 int main() {
-    FILE *fptr = NULL;
-    char line[MAX_LINE_LENGTH] = {0};
+    FILE *fptr = open_file();
+    char line[MAX_LINE_LENGTH];
 
-    open_file(fptr);
 
     while (fgets(line, MAX_LINE_LENGTH, fptr)) {
-        printf("%s", line);
+        char *student_data[15];
         char *filed = strtok(line, " ");
-
         // loop through the string to extract all others tokens
-        while (filed != NULL) {
-            printf(" %s\n", filed); //printing each token
+
+        for (int i = 0; filed != NULL; i++) {
+            student_data[i] = malloc(strlen(filed) + 1);
+            student_data[i] = filed;
             filed = strtok(NULL, " ");
         }
+
+        struct Student stud;
+        for (int i = 0, j = 4; i < NUM_COURSES; i++, j++) {
+            int grade = atoi(student_data[j]);
+            struct Course c = {"CourseName", grade, false};
+            stud._courses_list[i] = c;
+        }
+
+        stud._first_name = malloc(strlen(student_data[0]));
+        strcpy(stud._first_name, student_data[0]);
+        stud._last_name = malloc(strlen(student_data[1]));
+        strcpy(stud._last_name, student_data[1]);
+        stud._phone_number = malloc(strlen(student_data[2]));
+        strcpy(stud._phone_number, student_data[2]);
+
     }
 
     fclose(fptr);
